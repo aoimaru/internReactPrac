e250be3c0e07c92280ba564e0388d2530bfd779e
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-06-07 11:25:38 +0900

    直接書き込み(文字色変更)

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import colorSample from "./components/colorSample";
 function App() {
   return (
     <div className="App">
-      <h1> Hello React</h1>
+      <h1> Hello React (タグ)</h1>
+
+      <div style={{ color: 'red' }}>文字の色を変更(直接書き込み)</div>
+
       <colorSample color="blue" message="元気です">
         hello
       </colorSample>
